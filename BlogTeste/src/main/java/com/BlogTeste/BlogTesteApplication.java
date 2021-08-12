4a3ba2177e4062ba223e120af60e76e34c4a296e
Gui Matheus <guilherme.maths.sil@gmail.com>
2021-08-12 17:58:44 -0300

    up de documentacao pdf do Swagger

--- BlogTeste/src/main/java/com/BlogTeste/BlogTesteApplication.java
+++ BlogTeste/src/main/java/com/BlogTeste/BlogTesteApplication.java
@@ -2,12 +2,24 @@ package com.BlogTeste;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
-public class BlogTesteApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BlogTesteApplication.class, args);
+@RestController
+@RequestMapping("/")
+public class BlogTesteApplication
+{
+	@GetMapping
+	public ModelAndView swaggerUi()
+	{
+		return new ModelAndView("redirect:/swagger-ui/");
 	}
 
+	public static void main(String[] args)
+	{
+		SpringApplication.run(BlogTesteApplication.class, args);
+	}
 }
--- BlogTeste/src/main/java/com/BlogTeste/seguranca/BasicSecurityConfig.java
+++ BlogTeste/src/main/java/com/BlogTeste/seguranca/BasicSecurityConfig.java
@@ -26,7 +26,8 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter
 		auth.inMemoryAuthentication()
 		.withUser("admin").password(passwordEncorder().encode("admin")).authorities("ROLE_ADMIN");
 		
-		auth.userDetailsService(service);
+		
+
 	}
 	
 	@Bean
